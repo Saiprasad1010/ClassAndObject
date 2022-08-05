a356e873cbc5d2a42d066b270535de78106c44ad
Saiprasad <saiprasadpatil1010@gmail.com>
2022-08-05 22:05:48 +0530

    first commit

---
+++ src/Revision/ClassAndObjects.java
@@ -0,0 +1,50 @@
+package Revision;
+
+public class ClassAndObjects {
+	
+	byte a=12;
+	
+	int b=25;
+	int c=95;
+	int d=b+c;
+	
+	short e=15230;
+	
+	float f=13.25f;
+	
+	double g=13.2569;
+	
+	long h=1230658;
+	
+	boolean i= true;
+	
+	 char k='z';
+	 
+	 int m=36;
+	  int n=85;
+	  int o=m*n;
+	  int p=n/m;
+	  int q=n-m;
+	  int r=m%n;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		ClassAndObjects o=new ClassAndObjects();
+		System.out.println(o.a);
+		System.out.println(o.d);
+		System.out.println(o.e);
+		System.out.println(o.f);
+		System.out.println(o.g);
+		System.out.println(o.h);
+		System.out.println(o.i);
+		System.out.println(o.k);
+		System.out.println(o.o);
+		System.out.println(o.p);
+		System.out.println(o.q);
+		System.out.println(o.r);
+		
+	}
+	
+
+}
